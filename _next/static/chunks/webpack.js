375ffeac15701ae0da1d2b0448f7777df4654de9
viresh <veeresh@amagi.com>
2021-01-03 17:20:15 +0530

    increase title size

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5baa39ed23027ab2cbfb";
+/******/ 	var hotCurrentHash = "cdb716adf4591965b288";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
