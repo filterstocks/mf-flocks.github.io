9e07ccd1169471338fefd2333c18c0c5cfeb59bb
viresh <veeresh@amagi.com>
2021-01-03 13:12:50 +0530

    remove unwanted margin at the top. Loving front-end work now. Fear-- now

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e7e0ac6930fed2ee7aa3";
+/******/ 	var hotCurrentHash = "5baa39ed23027ab2cbfb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
