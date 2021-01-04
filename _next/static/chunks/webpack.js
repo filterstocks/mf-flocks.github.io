43a52ad08f7cb44b4085f833ce6d40456cb027d7
viresh <veeresh@amagi.com>
2021-01-04 13:24:36 +0530

    remove padding

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9736c545d1040109ccaa";
+/******/ 	var hotCurrentHash = "f151e889a98a0b2e0cad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
