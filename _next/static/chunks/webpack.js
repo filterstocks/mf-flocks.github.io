3ba6c3b5a4948a36da113c69ad7d43e44a15f1ef
viresh <veeresh@amagi.com>
2021-01-06 11:54:09 +0530

    fix header

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "70799a7b53a3a0334e58";
+/******/ 	var hotCurrentHash = "deb9ddf9c71845f67233";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
