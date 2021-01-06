9588c58ad04d17da9b0fd74a00fe6f9b2e41f3fa
viresh <veeresh@amagi.com>
2021-01-06 20:41:44 +0530

    got to be responsive in 2021

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ba28959b12b9043bd036";
+/******/ 	var hotCurrentHash = "db16430808dafab7abeb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
