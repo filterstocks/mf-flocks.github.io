82e0b898444d02d852faefbf133f8c0f00fcf62c
viresh <veeresh@amagi.com>
2021-01-06 12:44:47 +0530

    final final commit :fingers-crosse

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "77d7f1316f8eeb9228cc";
+/******/ 	var hotCurrentHash = "b27921740d52bc3da628";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
