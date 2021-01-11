38236c303565ab591f26fca7d0e34d4581df07e9
viresh <veeresh@amagi.com>
2021-01-11 20:13:14 +0530

    can subscribe to mailing list now

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d63da8b395be54048683";
+/******/ 	var hotCurrentHash = "351f09b4e12013643db4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
