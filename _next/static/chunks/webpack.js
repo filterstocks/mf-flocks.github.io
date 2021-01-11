5e36ac395cf436beb9d4f68390759c3407e6a90b
viresh <veeresh@amagi.com>
2021-01-11 12:49:29 +0530

    change font

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "68f0b633bed371686966";
+/******/ 	var hotCurrentHash = "abeb5644c513304320d4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
